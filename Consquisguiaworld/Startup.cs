94290fbeff97221631128c5bd71f0c2ab69b4d09
Marino Rosario Portorreal <marino_rosario1@ucne.edu.do>
2018-06-04 00:27:38 -0400

    Inicio del proyecto con el manejo de usuarios

---
+++ Consquisguiaworld/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Consquisguiaworld.Startup))]
+namespace Consquisguiaworld
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
